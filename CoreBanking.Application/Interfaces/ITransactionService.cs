d43fe78ec81a98d77f5ae129ea11bfa0330cc50c
Muhammad idris <idrismuhd814@gmail.com>
2025-10-11 12:45:24 +0100

    Added unit of work

---
+++ CoreBanking.Application/Interfaces/ITransactionService.cs
@@ -0,0 +1,15 @@
+﻿using CoreBanking.DTOs.TransactionDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBanking.Application.Interfaces
+{
+    public interface ITransactionService
+    {
+        Task<TransactionResponseDto> DepositAsync(Guid adminId, DepositRequestDto request);
+        Task<TransactionResponseDto> WithdrawAsync(Guid userId, WithdrawalRequestDto request);
+    }
+}
---
+++ CoreBanking.DTOs/TransactionDto/TransactionResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBanking.DTOs.TransactionDto
+{
+    public class TransactionResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = null!; 
+        public string? Reference { get; set; }
+        public decimal? NewBalance { get; set; }
+    }
+}
